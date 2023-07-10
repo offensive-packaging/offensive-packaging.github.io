e1d9ccbce7e5c104bb9d35c8cfb7da8717b4061a
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-10 16:47:26 +0000

    up

---
+++ src/containers/ContributeContainers/GetInvolvedSection/contribute.ts
@@ -0,0 +1,11 @@
+import Github from 'containers/ContributeContainers/assets/github.svg'
+
+export default [
+  {
+    title: 'GitHub - backup mirror',
+    description:
+      'The repository where we keep a copy of the most important code in case our servers are down.',
+    url: 'https://github.com/offensive-packaging',
+    icon: Github
+  }
+]
